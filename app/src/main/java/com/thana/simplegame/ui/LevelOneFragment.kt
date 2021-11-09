f761d71990857516149b4bd65a2413068e501370
ThanaCS <Thana_CS@hotmail.com>
2021-11-09 12:48:43 +0300

    Add polish for level one

--- app/src/main/java/com/thana/simplegame/ui/LevelOneFragment.kt
+++ app/src/main/java/com/thana/simplegame/ui/LevelOneFragment.kt
@@ -1,33 +1,72 @@
 package com.thana.simplegame.ui
 
 import android.annotation.SuppressLint
-import android.graphics.Canvas
-import android.graphics.Paint
 import android.os.Bundle
 import android.view.DragEvent
 import android.view.MotionEvent
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.upstream.*
 import com.thana.simplegame.R
 import com.thana.simplegame.databinding.FragmentLevelOneBinding
 import com.thana.simplegame.ui.common.BaseFragment
 import com.thana.simplegame.ui.common.viewBinding
 import com.thana.simplegame.util.hideKeyboard
 
+
 class LevelOneFragment : BaseFragment(R.layout.fragment_level_one), View.OnTouchListener,
     View.OnDragListener {
 
     private val binding by viewBinding(FragmentLevelOneBinding::bind)
+
+    private lateinit var winAudio: ExoPlayer
+    private lateinit var loseAudio: ExoPlayer
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.next.setOnClickListener {
             nextLevel()
         }
+
         setListeners()
+        showHint()
         validateAnswer()
 
+    }
+
+    private fun showHint(){
+        binding.expand.setOnClickListener {
+            binding.hint.visibility =View.VISIBLE
+            binding.collapse.visibility = View.VISIBLE
+            binding.expand.visibility = View.INVISIBLE
+        }
+        binding.collapse.setOnClickListener {
+            binding.hint.visibility =View.GONE
+            binding.collapse.visibility = View.INVISIBLE
+            binding.expand.visibility = View.VISIBLE
+        }
+    }
+    private fun initAudio() {
+
+        winAudio = ExoPlayer.Builder(requireContext()).build()
+        loseAudio = ExoPlayer.Builder(requireContext()).build()
+
+        val winUri = RawResourceDataSource.buildRawResourceUri(R.raw.win)
+        val loseUri = RawResourceDataSource.buildRawResourceUri(R.raw.lose)
+
+        winAudio.apply {
+            setMediaItem(MediaItem.fromUri(winUri))
+            prepare()
+
+        }
+
+        loseAudio.apply {
+            setMediaItem(MediaItem.fromUri(loseUri))
+            prepare()
+        }
     }
 
     private fun nextLevel() {
@@ -58,6 +97,7 @@ class LevelOneFragment : BaseFragment(R.layout.fragment_level_one), View.OnTouch
     private fun validateAnswer() {
 
         binding.submit.setOnClickListener {
+            initAudio()
             val input = binding.editText.text.toString().trim()
             if (input == "7") {
                 binding.right.visibility = View.VISIBLE
@@ -65,9 +105,17 @@ class LevelOneFragment : BaseFragment(R.layout.fragment_level_one), View.OnTouch
                 binding.celebrate.visibility = View.VISIBLE
                 binding.celebrate.playAnimation()
                 binding.submit.isEnabled = false
+                binding.next.visibility = View.VISIBLE
+
+                if(::winAudio.isInitialized)
+                    winAudio.play()
+
             } else {
                 binding.wrong.visibility = View.VISIBLE
                 binding.right.visibility = View.GONE
+
+                if(::loseAudio.isInitialized)
+                    loseAudio.play()
             }
             hideKeyboard()
         }
